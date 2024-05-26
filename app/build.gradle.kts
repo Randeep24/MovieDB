15f1aac28385b9ee0f7304a29b27b846ddcfe0cb
Randeep Kaur <randeep.kaur@later.com>
2024-05-25 22:42:40 -0700

    Adding Hilt Library and packages

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
         alias(libs.plugins.android.application)
         alias(libs.plugins.jetbrains.kotlin.android)
+        alias(libs.plugins.jetbrains.kotlin.kapt)
+        alias(libs.plugins.gradle.hilt)
 }
 
 android {
@@ -27,11 +29,12 @@ android {
                 }
         }
         compileOptions {
-                sourceCompatibility = JavaVersion.VERSION_1_8
-                targetCompatibility = JavaVersion.VERSION_1_8
+                sourceCompatibility = JavaVersion.VERSION_17
+                targetCompatibility = JavaVersion.VERSION_17
         }
+
         kotlinOptions {
-                jvmTarget = "1.8"
+                jvmTarget = "17"
         }
 }
 
@@ -42,6 +45,11 @@ dependencies {
         implementation(libs.material)
         implementation(libs.androidx.activity)
         implementation(libs.androidx.constraintlayout)
+
+        implementation(libs.hilt.android)
+        kapt(libs.hilt.compiler)
+
+
         testImplementation(libs.junit)
         androidTestImplementation(libs.androidx.junit)
         androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/randeep/moviedb/MovieDbApplication.kt
@@ -0,0 +1,7 @@
+package com.randeep.moviedb
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MovieDbApplication: Application()
